ed701a2248a65c80136f6e0eba8c8206b899a5fc
dominiclaserna <lblaserna@up.edu.ph>
2021-03-05 07:37:25 +0800

    My commit with the css file

---
+++ dom.css
@@ -0,0 +1,37 @@
+body {
+  background-color: lightblue;
+}p.dotted {border-style: dotted;}
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;
+}
+
+
+h1 {
+  color: green;
+  text-align: center;font-size: 70px;
+
+}
+h2 {
+  color: yellow;
+  text-align: center;font-size: 60px;
+}h3 {
+  color: pink;
+  text-align: center;font-size: 50px;
+}h4 {
+  color: orange;
+  text-align: center;font-size: 40px;
+}h5 {
+  color: violet;
+  text-align: center;font-size: 40px;
+}h6 {
+  color: white;
+  text-align: center;font-size: 40px;
+}
+
+p {
+  font-family: verdana;
+  font-size: 20px;
+}
